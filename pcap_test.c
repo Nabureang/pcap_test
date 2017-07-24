3be541277e258f5422c12e9b7ca71ce917efd39e
rkd500 <rkdtjdcjf700@gmail.com>
2017-07-24 01:01:40 -0700

    Fix pcap_test.c

--- pcap_test.c
+++ pcap_test.c
@@ -1,28 +1,72 @@
 #include "pcap_test.h"
-#include <stdio.h>
-void printEtherAddress(struct ether_header *eh)
+
+void printEtherAddress(struct sniff_ethernet *eh)
 {
 	printf("Destination MAC Address : ");
 	for(int i = 0 ; i < 6 ; i ++)
-		printf("%02x%c", eh->ether_dhost.ether_addr_octet[i], (i == 5) ? '\n' : ':');
+		printf("%02x%c", eh->ether_dhost[i], (i == 5) ? '\n' : ':');
 	printf("Source MAC Address : ");
 	for(int i = 0 ; i < 6 ; i ++)
-		printf("%02x%c", eh->ether_shost.ether_addr_octet[i], (i == 5) ? '\n' : ':');
+		printf("%02x%c", eh->ether_shost[i], (i == 5) ? '\n' : ':');
 }
 
-void printIpAddress(struct ip_header *ip)
+void printIpAddress(struct sniff_ip *ih)
 {
-	printf("Destinsation IP Address : ");
-	for(int i = 0 ; i < 4 ; i ++)
-		printf("%d%c", ip->ip_destaddr.ip_addr_octet[i], (i == 3) ? '\n' : '.');
-	printf("Source IP Address : ");
-	for(int i = 0 ; i < 4 ; i ++)
-		printf("%d%c", ip->ip_srcaddr.ip_addr_octet[i], (i == 3) ? '\n' : '.');
+	char str[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(ih->ip_dst), str, INET_ADDRSTRLEN);
+	printf("Destinsation IP Address : %s\n", str );
+	inet_ntop(AF_INET, &(ih->ip_src), str, INET_ADDRSTRLEN);
+	printf("Source IP Address : %s\n", str );
 } 
-void printTcpAddressAndData(struct tcp_header *tcp)
+
+void printTcpAddress(struct sniff_tcp *th)
 {
-	printf("Destination Port : %d\nSource Port : %d\n", tcp->tcp_destport,
-									 tcp->tcp_srcport);
-	for(int i = 0 ; i < 100 ; i ++) // print data. \n when 16, 32, 48... ,  99
-		printf("%02x%c%c", tcp->tcp_data[i], ((i+1) % 16 == 0) ? '\n' : ' ', (i==99) ? '\n' : '\0');
+	printf("Destination Port : %d\nSource Port : %d\n", ntohs(th->th_dport),
+							 ntohs(th->th_sport));
 }
+
+void printData(u_char *data, uint16_t length)
+{
+	int packet_num_line = 0;
+	u_char *tmp = data;
+	for(int i = 0 ; i < length ; i ++)
+	{
+		
+		packet_num_line ++;
+
+		printf("%02x ", *(data+i));
+		
+		if((i+1) % 16 == 0) // print data in char type
+		{
+			putchar('|');
+			for(int j = 0 ; j < packet_num_line ; j ++)
+			{ 	
+				if(*(tmp+j) == 0x0d || *(tmp+j) == 0x0a  )
+					putchar('.');
+				else
+					printf("%0c", *(tmp+j));
+			}
+			putchar('\n');
+			tmp = data+i+1;
+			packet_num_line = 0;
+		}
+	}
+	if(packet_num_line != 0) // print extra last line data in char type
+	{
+		for(int k = 0 ; k < 16-packet_num_line ; k++)
+		{
+			printf("   ");
+		} 
+		putchar('|');
+		for(int j = 0 ; j < packet_num_line ; j ++)
+		{ 	
+			if(*(tmp+j) == 0x0d || *(tmp+j) == 0x0a  )
+				putchar('.');
+			else
+				printf("%0c", *(tmp+j));
+		}
+		putchar('\n');
+	}
+		
+}
+	
